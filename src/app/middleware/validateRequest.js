b83b7729d6b41f8315737d07bf79d13f551452d5
Sani-Mohibur <mohiburrahmansani@gmail.com>
2026-08-03 18:00:34 +0600

    chore: cleanup code

---
+++ src/app/middleware/validateRequest.js
@@ -0,0 +1,13 @@
+import catchAsync from '../utils/catchAsync.js';
+
+const validateRequest = (schema) => {
+  return catchAsync(async (req, res, next) => {
+    await schema.parseAsync({
+      body: req.body,
+      cookies: req.cookies,
+    });
+    next();
+  });
+};
+
+export default validateRequest;
